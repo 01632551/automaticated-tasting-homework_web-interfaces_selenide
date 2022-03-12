8c7e42e455be0159481c21994deca7f4aa69d932
Egor) <kes2079531@gmail.com>
2022-03-12 21:09:41 +0300

    update tests & appveyor

--- src/test/java/ru/netology/delivery/PositiveCardDeliveryDateTest.java
+++ src/test/java/ru/netology/delivery/PositiveCardDeliveryDateTest.java
@@ -1,87 +1,75 @@
 package ru.netology.delivery;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PositiveCardDeliveryDateTest {
 
-    public String needDate(LocalDate localDate) {
-        int dayOfMonth = localDate.getDayOfMonth();
-        int month = localDate.getMonthValue();
-        int year = localDate.getYear();
-        String rightDate;
-        if (dayOfMonth < 10 && month < 10) {
-            return String.format("0%d.0%d.%d", dayOfMonth, month, year);
-        }
-        if (dayOfMonth < 10 && month >=10) {
-            return String.format("0%d.%d.%d", dayOfMonth, month, year);
-        }
-        if (dayOfMonth >= 10 && month < 10) {
-            return String.format("%d.0%d.%d", dayOfMonth, month, year);
-        } else {
-            return String.format("%d.%d.%d", dayOfMonth, month, year);
-        }
+    String generateDate(int days) {
+
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
     }
 
     // TEST 1: DATE ON THE BOARD OF ALLOWABLE VALUE (+3 days after delivery date)
     @Test
     public void shouldLoadFormV1() {
-        LocalDate localDate = LocalDate.now();
-        localDate = localDate.plusDays(3);
 
-        open("http://localhost:7777/");
+        int days = 3;
+
+        open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Майкоп");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").doubleClick().setValue(needDate(localDate));
+        $("[data-test-id=date] input").doubleClick().setValue(generateDate(days));
         $("[data-test-id=name] input").setValue("Немирович-Данченко Даниил");
         $("[data-test-id=phone] input").setValue("+79654321789");
         $("[data-test-id=agreement] .checkbox__box").click();
         $(".button").click();
         $("[data-test-id=notification]").shouldBe(Condition.appear, Duration.ofSeconds(15));
-        $("[data-test-id=notification] .notification__content").shouldHave(Condition.ownText(needDate(localDate)));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.ownText(generateDate(days)));
     }
 
     // TEST 2: A LONG TIME AFTER DATE OF DELIVERY (+ one year after delivery date)
     @Test
     public void shouldLoadFormV2() {
-        LocalDate localDate = LocalDate.now();
-        localDate = localDate.plusYears(1);
 
-        open("http://localhost:7777/");
+        int days = 365;
+
+        open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Улан-Удэ");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").doubleClick().setValue(needDate(localDate));
+        $("[data-test-id=date] input").doubleClick().setValue(generateDate(days));
         $("[data-test-id=name] input").setValue("Крылов ИИ");
         $("[data-test-id=phone] input").setValue("+79654321789");
         $("[data-test-id=agreement] .checkbox__box").click();
         $(".button").click();
         $("[data-test-id=notification]").shouldBe(Condition.appear, Duration.ofSeconds(15));
-        $("[data-test-id=notification] .notification__content").shouldHave(Condition.ownText(needDate(localDate)));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.ownText(generateDate(days)));
     }
 
     // TEST 3: AVERAGE TIME AFTER A DATE OF DELIVERY (+ 2 month after delivery date)
     @Test
     public void shouldLoadFormV3() {
-        LocalDate localDate = LocalDate.now();
-        localDate = localDate.plusMonths(2);
 
-        open("http://localhost:7777/");
+        int days = 61;
+
+        open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").doubleClick().setValue(needDate(localDate));
+        $("[data-test-id=date] input").doubleClick().setValue(generateDate(days));
         $("[data-test-id=name] input").setValue("Крючкова Софья");
         $("[data-test-id=phone] input").setValue("+79654321789");
         $("[data-test-id=agreement] .checkbox__box").click();
         $(".button").click();
         $("[data-test-id=notification]").shouldBe(Condition.appear, Duration.ofSeconds(15));
-        $("[data-test-id=notification] .notification__content").shouldHave(Condition.ownText(needDate(localDate)));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.ownText(generateDate(days)));
     }
 }
--- src/test/java/ru/netology/delivery/PositiveCardDeliveryNameTest.java
+++ src/test/java/ru/netology/delivery/PositiveCardDeliveryNameTest.java
@@ -1,4 +1,5 @@
 package ru.netology.delivery;
+
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
@@ -6,91 +7,82 @@ import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PositiveCardDeliveryNameTest {
 
-    public String needDate () {
-        LocalDate localDate = LocalDate.now();
-        localDate = localDate.plusDays(7);
-        int dayOfMonth = localDate.getDayOfMonth();
-        int month = localDate.getMonthValue();
-        int year = localDate.getYear();
-        String rightDate;
-        if (dayOfMonth < 10 && month < 10) {
-            return String.format("0%d.0%d.%d", dayOfMonth, month, year);
-        }
-        if (dayOfMonth < 10 && month >=10) {
-            return String.format("0%d.%d.%d", dayOfMonth, month, year);
-        }
-        if (dayOfMonth >= 10 && month < 10) {
-            return String.format("%d.0%d.%d", dayOfMonth, month, year);
-        } else {
-            return String.format("%d.%d.%d", dayOfMonth, month, year);
-        }
+    String generateDate() {
+
+        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
     }
 
     // (SMP = simple, CMP = compound)
 
-// TEST 1: SMP SURNAME & NAME
+    // TEST 1: SMP SURNAME & NAME
     @Test
-    public void shouldLoadFormV1 (){
+    public void shouldLoadFormV1() {
 
-        open("http://localhost:7777/");
+        open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").doubleClick().setValue(needDate());
+        $("[data-test-id=date] input").doubleClick().setValue(generateDate());
         $("[data-test-id=name] input").setValue("Крючкова Софья");
         $("[data-test-id=phone] input").setValue("+79654321789");
         $("[data-test-id=agreement] .checkbox__box").click();
         $(".button").click();
         $("[data-test-id=notification]").shouldBe(Condition.appear, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.ownText(generateDate()));
     }
 
-// TEST 2: CMP SURNAME & SMP NAME
+    // TEST 2: CMP SURNAME & SMP NAME
     @Test
-    public void shouldLoadFormV2 (){
+    public void shouldLoadFormV2() {
 
-        open("http://localhost:7777/");
+        open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Майкоп");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").doubleClick().setValue(needDate());
+        $("[data-test-id=date] input").doubleClick().setValue(generateDate());
         $("[data-test-id=name] input").setValue("Немирович-Данченко Даниил");
         $("[data-test-id=phone] input").setValue("+79159635457");
         $("[data-test-id=agreement] .checkbox__box").click();
         $(".button").click();
         $("[data-test-id=notification]").shouldBe(Condition.appear, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.ownText(generateDate()));
     }
 
-// TEST 3: SMP SURNAME & CMP NAME
+    // TEST 3: SMP SURNAME & CMP NAME
     @Test
-    public void shouldLoadFormV3 (){
+    public void shouldLoadFormV3() {
 
-        open("http://localhost:7777/");
+        open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Йошкар-Ола");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").doubleClick().setValue(needDate());
+        $("[data-test-id=date] input").doubleClick().setValue(generateDate());
         $("[data-test-id=name] input").setValue("Ефанова Анна-Мария");
         $("[data-test-id=phone] input").setValue("+79369704560");
         $("[data-test-id=agreement] .checkbox__box").click();
         $(".button").click();
         $("[data-test-id=notification]").shouldBe(Condition.appear, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.ownText(generateDate()));
     }
 
-// TEST 4: CMP SURNAME & СMP NAME
+    // TEST 4: CMP SURNAME & СMP NAME
     @Test
-    public void shouldLoadFormV4 (){
+    public void shouldLoadFormV4() {
 
-        open("http://localhost:7777/");
+        open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Кызыл");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").doubleClick().setValue(needDate());
+        $("[data-test-id=date] input").doubleClick().setValue(generateDate());
         $("[data-test-id=name] input").setValue("Иванова-Зыкова Ирина-Любовь");
         $("[data-test-id=phone] input").setValue("+78005553535");
         $("[data-test-id=agreement] .checkbox__box").click();
         $(".button").click();
         $("[data-test-id=notification]").shouldBe(Condition.appear, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.ownText(generateDate()));
     }
 }
